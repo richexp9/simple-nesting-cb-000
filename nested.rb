8c247e1a1e4487ff8bf558bb404117429ecc5454
Richard Loera <richexp9@gmx.com>
2018-01-16 19:20:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -69,10 +69,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto] = {
-	:known_for => "Ruby",
-  :languages => ["LISP", "C"]
-}
+
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -88,7 +85,10 @@ programmer_hash[:yukihiro_matsumoto] = {
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto] = {
+		 	:known_for => "Ruby",
+		   :languages => ["LISP", "C"]
+		 }
 
 end
 
